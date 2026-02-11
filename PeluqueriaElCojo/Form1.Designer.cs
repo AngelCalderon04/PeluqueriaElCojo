1a924e40b32c9e5ae5711b319e5bcc98b50cb6c3
ANGEL LUIS CALDERON DEL ROSARIO <angelcalderon687@gmail.com>
2026-02-10 21:26:35 -0400

    Diseno Form

--- PeluqueriaElCojo/Form1.Designer.cs
+++ PeluqueriaElCojo/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PeluqueriaElCojo
 {
-    partial class Form1
+    partial class peluqueriaelcojo
     {
         /// <summary>
         /// Required designer variable.
@@ -43,7 +43,13 @@
             this.btnCobrar = new System.Windows.Forms.Button();
             this.txtRecibo = new System.Windows.Forms.TextBox();
             this.lblTotal = new System.Windows.Forms.Label();
+            this.CLIENTES = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.numNivel)).BeginInit();
+            this.CLIENTES.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // contextMenuStrip1
@@ -54,7 +60,7 @@
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(12, 114);
+            this.txtNombre.Location = new System.Drawing.Point(9, 43);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(201, 31);
             this.txtNombre.TabIndex = 2;
@@ -62,16 +68,16 @@
             // 
             // txtTelefono
             // 
-            this.txtTelefono.Location = new System.Drawing.Point(12, 178);
+            this.txtTelefono.Location = new System.Drawing.Point(9, 107);
             this.txtTelefono.Name = "txtTelefono";
-            this.txtTelefono.Size = new System.Drawing.Size(201, 26);
+            this.txtTelefono.Size = new System.Drawing.Size(201, 34);
             this.txtTelefono.TabIndex = 3;
             // 
             // btnAgregarCliente
             // 
-            this.btnAgregarCliente.Location = new System.Drawing.Point(72, 263);
+            this.btnAgregarCliente.Location = new System.Drawing.Point(46, 179);
             this.btnAgregarCliente.Name = "btnAgregarCliente";
-            this.btnAgregarCliente.Size = new System.Drawing.Size(89, 34);
+            this.btnAgregarCliente.Size = new System.Drawing.Size(121, 48);
             this.btnAgregarCliente.TabIndex = 4;
             this.btnAgregarCliente.Text = "Agregar";
             this.btnAgregarCliente.UseVisualStyleBackColor = true;
@@ -80,8 +86,8 @@
             // lstClientes
             // 
             this.lstClientes.FormattingEnabled = true;
-            this.lstClientes.ItemHeight = 20;
-            this.lstClientes.Location = new System.Drawing.Point(12, 337);
+            this.lstClientes.ItemHeight = 28;
+            this.lstClientes.Location = new System.Drawing.Point(9, 266);
             this.lstClientes.Name = "lstClientes";
             this.lstClientes.Size = new System.Drawing.Size(230, 144);
             this.lstClientes.TabIndex = 5;
@@ -90,9 +96,9 @@
             // chkCorteNormal
             // 
             this.chkCorteNormal.AutoSize = true;
-            this.chkCorteNormal.Location = new System.Drawing.Point(338, 114);
+            this.chkCorteNormal.Location = new System.Drawing.Point(14, 45);
             this.chkCorteNormal.Name = "chkCorteNormal";
-            this.chkCorteNormal.Size = new System.Drawing.Size(202, 24);
+            this.chkCorteNormal.Size = new System.Drawing.Size(206, 25);
             this.chkCorteNormal.TabIndex = 6;
             this.chkCorteNormal.Text = "Corte Normal (RD$200)";
             this.chkCorteNormal.UseVisualStyleBackColor = true;
@@ -100,16 +106,16 @@
             // chkDegradado
             // 
             this.chkDegradado.AutoSize = true;
-            this.chkDegradado.Location = new System.Drawing.Point(338, 153);
+            this.chkDegradado.Location = new System.Drawing.Point(14, 87);
             this.chkDegradado.Name = "chkDegradado";
-            this.chkDegradado.Size = new System.Drawing.Size(115, 24);
+            this.chkDegradado.Size = new System.Drawing.Size(118, 25);
             this.chkDegradado.TabIndex = 7;
             this.chkDegradado.Text = "Degradado";
             this.chkDegradado.UseVisualStyleBackColor = true;
             // 
             // numNivel
             // 
-            this.numNivel.Location = new System.Drawing.Point(459, 152);
+            this.numNivel.Location = new System.Drawing.Point(139, 83);
             this.numNivel.Maximum = new decimal(new int[] {
             5,
             0,
@@ -121,20 +127,21 @@
             0,
             0});
             this.numNivel.Name = "numNivel";
-            this.numNivel.Size = new System.Drawing.Size(54, 26);
+            this.numNivel.Size = new System.Drawing.Size(54, 29);
             this.numNivel.TabIndex = 8;
             this.numNivel.Value = new decimal(new int[] {
             1,
             0,
             0,
             0});
+            this.numNivel.ValueChanged += new System.EventHandler(this.numNivel_ValueChanged);
             // 
             // chkAfeitado
             // 
             this.chkAfeitado.AutoSize = true;
-            this.chkAfeitado.Location = new System.Drawing.Point(353, 208);
+            this.chkAfeitado.Location = new System.Drawing.Point(14, 134);
             this.chkAfeitado.Name = "chkAfeitado";
-            this.chkAfeitado.Size = new System.Drawing.Size(169, 24);
+            this.chkAfeitado.Size = new System.Drawing.Size(168, 25);
             this.chkAfeitado.TabIndex = 9;
             this.chkAfeitado.Text = "Afeitado (RD$150)";
             this.chkAfeitado.UseVisualStyleBackColor = true;
@@ -142,9 +149,9 @@
             // chkToalla
             // 
             this.chkToalla.AutoSize = true;
-            this.chkToalla.Location = new System.Drawing.Point(351, 260);
+            this.chkToalla.Location = new System.Drawing.Point(14, 181);
             this.chkToalla.Name = "chkToalla";
-            this.chkToalla.Size = new System.Drawing.Size(155, 24);
+            this.chkToalla.Size = new System.Drawing.Size(154, 25);
             this.chkToalla.TabIndex = 10;
             this.chkToalla.Text = "+ Toalla (RD$50)";
             this.chkToalla.UseVisualStyleBackColor = true;
@@ -152,65 +159,115 @@
             // chkCejas
             // 
             this.chkCejas.AutoSize = true;
-            this.chkCejas.Location = new System.Drawing.Point(358, 313);
+            this.chkCejas.Location = new System.Drawing.Point(14, 234);
             this.chkCejas.Name = "chkCejas";
-            this.chkCejas.Size = new System.Drawing.Size(140, 24);
+            this.chkCejas.Size = new System.Drawing.Size(136, 25);
             this.chkCejas.TabIndex = 11;
             this.chkCejas.Text = "Cejas (RD$75)";
             this.chkCejas.UseVisualStyleBackColor = true;
             // 
             // btnCobrar
             // 
-            this.btnCobrar.Location = new System.Drawing.Point(378, 380);
+            this.btnCobrar.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnCobrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCobrar.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCobrar.ForeColor = System.Drawing.Color.White;
+            this.btnCobrar.Location = new System.Drawing.Point(57, 321);
             this.btnCobrar.Name = "btnCobrar";
-            this.btnCobrar.Size = new System.Drawing.Size(107, 26);
+            this.btnCobrar.Size = new System.Drawing.Size(125, 36);
             this.btnCobrar.TabIndex = 12;
             this.btnCobrar.Text = "COBRAR";
-            this.btnCobrar.UseVisualStyleBackColor = true;
+            this.btnCobrar.UseVisualStyleBackColor = false;
             this.btnCobrar.Click += new System.EventHandler(this.btnCobrar_Click);
             // 
             // txtRecibo
             // 
-            this.txtRecibo.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtRecibo.Location = new System.Drawing.Point(579, 106);
+            this.txtRecibo.BackColor = System.Drawing.Color.White;
+            this.txtRecibo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtRecibo.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRecibo.ForeColor = System.Drawing.Color.Black;
+            this.txtRecibo.Location = new System.Drawing.Point(6, 43);
             this.txtRecibo.Multiline = true;
             this.txtRecibo.Name = "txtRecibo";
             this.txtRecibo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtRecibo.Size = new System.Drawing.Size(341, 321);
+            this.txtRecibo.Size = new System.Drawing.Size(398, 387);
             this.txtRecibo.TabIndex = 13;
             // 
             // lblTotal
             // 
             this.lblTotal.AutoSize = true;
-            this.lblTotal.Location = new System.Drawing.Point(717, 401);
+            this.lblTotal.Location = new System.Drawing.Point(131, 402);
             this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(109, 20);
+            this.lblTotal.Size = new System.Drawing.Size(128, 28);
             this.lblTotal.TabIndex = 14;
             this.lblTotal.Text = "TOTAL: RD$0";
             // 
-            // Form1
+            // CLIENTES
+            // 
+            this.CLIENTES.Controls.Add(this.lstClientes);
+            this.CLIENTES.Controls.Add(this.btnAgregarCliente);
+            this.CLIENTES.Controls.Add(this.txtTelefono);
+            this.CLIENTES.Controls.Add(this.txtNombre);
+            this.CLIENTES.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CLIENTES.Location = new System.Drawing.Point(3, 71);
+            this.CLIENTES.Name = "CLIENTES";
+            this.CLIENTES.Size = new System.Drawing.Size(274, 451);
+            this.CLIENTES.TabIndex = 15;
+            this.CLIENTES.TabStop = false;
+            this.CLIENTES.Text = "CLIENTES";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnCobrar);
+            this.groupBox2.Controls.Add(this.chkCejas);
+            this.groupBox2.Controls.Add(this.chkToalla);
+            this.groupBox2.Controls.Add(this.chkAfeitado);
+            this.groupBox2.Controls.Add(this.numNivel);
+            this.groupBox2.Controls.Add(this.chkDegradado);
+            this.groupBox2.Controls.Add(this.chkCorteNormal);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(301, 69);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(252, 438);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "SERVICIOS";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblTotal);
+            this.groupBox3.Controls.Add(this.txtRecibo);
+            this.groupBox3.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(598, 71);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(410, 436);
+            this.groupBox3.TabIndex = 17;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "FACTURA";
+            // 
+            // peluqueriaelcojo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(948, 493);
-            this.Controls.Add(this.lblTotal);
-            this.Controls.Add(this.txtRecibo);
-            this.Controls.Add(this.btnCobrar);
-            this.Controls.Add(this.chkCejas);
-            this.Controls.Add(this.chkToalla);
-            this.Controls.Add(this.chkAfeitado);
-            this.Controls.Add(this.numNivel);
-            this.Controls.Add(this.chkDegradado);
-            this.Controls.Add(this.chkCorteNormal);
-            this.Controls.Add(this.lstClientes);
-            this.Controls.Add(this.btnAgregarCliente);
-            this.Controls.Add(this.txtTelefono);
-            this.Controls.Add(this.txtNombre);
-            this.Name = "Form1";
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1101, 630);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.CLIENTES);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "peluqueriaelcojo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numNivel)).EndInit();
+            this.CLIENTES.ResumeLayout(false);
+            this.CLIENTES.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -230,6 +287,9 @@
         private System.Windows.Forms.Button btnCobrar;
         private System.Windows.Forms.TextBox txtRecibo;
         private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.GroupBox CLIENTES;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
 
--- PeluqueriaElCojo/Form1.cs
+++ PeluqueriaElCojo/Form1.cs
@@ -11,14 +11,14 @@ using PeluqueriaElCojo.Modelos;
 
 namespace PeluqueriaElCojo
 {
-    public partial class Form1 : Form
+    public partial class peluqueriaelcojo : Form
     {
 
         private List<Cliente> _clientes = new List<Cliente>();
         private List<Servicio> _servicios = new List<Servicio>();
         private Cliente _clienteActual = null;
 
-        public Form1()
+        public peluqueriaelcojo()
         {
             InitializeComponent();
         }
@@ -48,8 +48,102 @@ namespace PeluqueriaElCojo
 
         private void btnCobrar_Click(object sender, EventArgs e)
         {
-            if (lstClientes.SelectedIndex >= 0)
-                _clienteActual = _clientes[lstClientes.SelectedIndex];
+
+
+            try
+            {
+                if (_clienteActual == null)
+                {
+                    MessageBox.Show("cliente es null");
+                    return;
+                }
+
+                _servicios.Clear();
+
+                if (chkCorteNormal.Checked) _servicios.Add(new CorteNormal());
+                if (chkDegradado.Checked) _servicios.Add(new Degradado((int)numNivel.Value));
+                if (chkAfeitado.Checked) _servicios.Add(new Afeitado(chkToalla.Checked));
+                if (chkCejas.Checked) _servicios.Add(new CorteCejas());
+
+                MessageBox.Show("Servicios agregados: " + _servicios.Count);
+
+                txtRecibo.Text = GenerarRecibo();
+
+                lblTotal.Text = string.Format("TOTAL: RD${0:N2}", CalcularTotal());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR REAL: " + ex.Message);
+            }
+
+
+        }
+
+        private decimal CalcularTotal()
+        {
+            decimal sub = 0;
+
+            foreach (Servicio s in _servicios)
+                sub += s.CalcularPrecio();
+
+            return sub * (1 - _clienteActual.ObtenerDescuento()) * 1.18m;
+        }
+
+        private string GenerarRecibo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("╔═══════════════════════════════╗");
+            sb.AppendLine("║    PELUQUERÍA EL COJO         ║");
+            sb.AppendLine("║    Villa Mella, Sto. Domingo  ║");
+            sb.AppendLine("╠═══════════════════════════════╣");
+            sb.AppendLine(string.Format("║ Cliente: {0,-20}║", _clienteActual.Nombre));
+            sb.AppendLine(string.Format("║ Tipo: {0,-23}║", _clienteActual.Tipo));
+            sb.AppendLine("╠═══════════════════════════════╣");
+
+            decimal sub = 0;
+
+            foreach (Servicio s in _servicios)
+            {
+                sb.AppendLine(string.Format("║  {0,-27}║", s.GenerarLineaRecibo()));
+                sub += s.CalcularPrecio();
+            }
+
+            sb.AppendLine("╠═══════════════════════════════╣");
+            sb.AppendLine(string.Format("║ Subtotal:        RD${0,8:N0} ║", sub));
+
+            decimal desc = _clienteActual.ObtenerDescuento();
+            if (desc > 0)
+                sb.AppendLine(string.Format("║ Descuento ({0}%): -RD${1,6:N0} ║", desc * 100, sub * desc));
+
+            decimal baseI = sub * (1 - desc);
+            sb.AppendLine(string.Format("║ ITBIS (18%):      RD${0,7:N0} ║", baseI * 0.18m));
+            sb.AppendLine("╠═══════════════════════════════╣");
+            sb.AppendLine(string.Format("║ TOTAL:           RD${0,8:N0} ║", baseI * 1.18m));
+            sb.AppendLine("╚═══════════════════════════════╝");
+            sb.AppendLine("      ¡Gracias por su visita!");
+
+            return sb.ToString();
+        }
+
+        private void LimpiarChecks()
+        {
+            chkCorteNormal.Checked = false;
+            chkDegradado.Checked = false;
+            chkAfeitado.Checked = false;
+            chkToalla.Checked = false;
+            chkCejas.Checked = false;
+            numNivel.Value = 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numNivel_ValueChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
